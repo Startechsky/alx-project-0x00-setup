6e31785c803fc150331e5dcc96eb920902e6139c
Ayo <ayo@Emenikes-MacBook-Pro.local>
2025-07-19 14:19:56 +0100

    Task_2 Completed

---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about:React.FC = () => {
+  return (
+    <div className=" text-xl font-extralight">About Page</div>
+  )
+}
+
+export default about;
---
+++ pages/landing.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const landing:React.FC = () => {
+  return (
+    <div className=" text-xl font-extralight">Landing Page</div>
+  )
+}
+
+export default landing
